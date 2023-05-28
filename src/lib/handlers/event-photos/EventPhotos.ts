d3dedddbc6a777ae12309cda197e8d2e3887fec2
Kevin Ho <kevinho@redairship.com>
2023-05-29 03:55:41 +0800

    feat(event-photos): add event-photo api endpoint

---
+++ src/lib/handlers/event-photos/EventPhotos.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import type { NextApiRequest, NextApiResponse } from "next";
+import { saveImages, loadImages } from "./EventPhotosManager"
+import { multerMiddleware } from "../../clients/multer"
+
+const registerEventPhotoRoutes = (router: any) => {
+    postEventPhotos(router)
+    getEventPhotos(router)
+}
+
+const postEventPhotos = (router: any) => {
+    router.post(multerMiddleware, async (req: any, res: NextApiResponse) => {
+        if (req.files.length === 0) {
+            res.json({ data: []})
+        } else {
+            const savedFiles = await saveImages(req.files)
+            res.json({ data: savedFiles });
+        }
+    })
+}
+
+const getEventPhotos = (router: any) => {
+    router.get(async (req: NextApiRequest, res: NextApiResponse) => {
+        const files = await loadImages({range: 1})
+        res.json({ data: files})
+    })
+}
+
+
+export default registerEventPhotoRoutes
