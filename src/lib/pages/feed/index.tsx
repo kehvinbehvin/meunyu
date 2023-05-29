8e66cea5e7ea3af3700d7b7dd5d008a54831bcf6
Lucas Ng <lucasngyileang@gmail.com>
2023-05-29 20:19:12 +0800

    feat(feed): add sorting tabs

--- src/lib/pages/feed/index.tsx
+++ src/lib/pages/feed/index.tsx
@@ -2,21 +2,14 @@ import { Flex } from '@chakra-ui/react';
 import { NextSeo } from 'next-seo';
 
 import NavBar from '~/lib/components/common/NavBar';
-import FeedCard from '~/lib/components/event-feed/FeedCard';
-import UploadPhoto from '~/lib/components/event-feed/UploadPhoto';
-import { useReelsContext } from '~/lib/contexts/ReelsContext';
+import EventFeed from '~/lib/components/event-feed';
 
 export default function Feed() {
-  const { reels } = useReelsContext();
-
   return (
     <Flex flexDir="column">
       <NextSeo title="Home" />
       <NavBar />
-      {reels.map((reel) => (
-        <FeedCard reel={reel} />
-      ))}
-      <UploadPhoto />
+      <EventFeed />
     </Flex>
   );
 }
