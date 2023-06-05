cd8b87f3e7dfff4e9a282912620827f8dd860f3d
Lucas Ng <lucasngyileang@gmail.com>
2023-06-05 23:25:22 +0800

    fix(ci): move hotfix to top of config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable:
-    process.env.NODE_ENV === "development" ||
-    process.env.NODE_ENV === "preview" ||
-    process.env.NODE_ENV === "production",
-  // delete two lines above to enable PWA in production deployment
-  // add your own icons to public/manifest.json
-  // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-});
-
+// Vercel build fix
 if (
   process.env.LD_LIBRARY_PATH == null ||
   !process.env.LD_LIBRARY_PATH.includes(
@@ -19,6 +9,17 @@ if (
     process.env.PWD
   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
 }
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable:
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'preview' ||
+    process.env.NODE_ENV === 'production',
+  // delete two lines above to enable PWA in production deployment
+  // add your own icons to public/manifest.json
+  // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+});
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
