44cf6cb67bdd96fbc7cb4818241f6fbb15862a31
Lucas Ng <lucasngyileang@gmail.com>
2023-06-14 12:35:25 +0800

    feat(ui): create admin page

---
+++ src/lib/pages/admin/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from '@chakra-ui/react';
+
+import Layout from '~/lib/layout';
+
+export default function AdminPage() {
+  return (
+    <Layout>
+      <Text textAlign="center" fontSize="3xl" fontWeight="extrabold">
+        Admin Page
+      </Text>
+    </Layout>
+  );
+}
