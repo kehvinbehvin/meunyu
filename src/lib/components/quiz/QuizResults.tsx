3eaaa78130fb4ab01322f0adf9a298c7f035d3f2
Lucas Ng <lucasngyileang@gmail.com>
2023-07-13 15:31:57 +0800

    feat(quiz): add images

--- src/lib/components/quiz/QuizResults.tsx
+++ src/lib/components/quiz/QuizResults.tsx
@@ -1,15 +1,30 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Image, Text } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useMemo } from 'react';
 
 import PrimaryButton from '../common/PrimaryButton';
+import quiz0 from '~/assets/quiz/quiz-0.jpeg';
+import quiz100 from '~/assets/quiz/quiz-100.jpeg';
+import quiz50 from '~/assets/quiz/quiz-50.jpeg';
 
 export default function QuizResults({ score }: { score: number }) {
+  const imageSrc = useMemo(() => {
+    switch (score) {
+      case 0:
+        return quiz0.src;
+      case 50:
+        return quiz50.src;
+      case 100:
+        return quiz100.src;
+      default:
+        return quiz100.src;
+    }
+  }, [score]);
   return (
-    <Box my={9} textAlign="center">
-      <Text fontSize="sm" color="white" textAlign="center" my={3}>
-        Your score
-      </Text>
-      <Text fontSize="lg" color="white" textAlign="center" my={3}>
+    <Box my={5} textAlign="center">
+      <Image src={imageSrc} my={9} borderRadius={4} />
+      <Text>Your score</Text>
+      <Text fontSize="5xl" color="white" textAlign="center" my={3}>
         {score}%
       </Text>
       <Link href="/journey">
