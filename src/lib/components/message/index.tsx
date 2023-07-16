8310463bd2389f58e72dcabe701051bff1c5f1b5
Lucas Ng <lucasngyileang@gmail.com>
2023-07-16 18:42:37 +0800

    feat(card): add save as photo

--- src/lib/components/message/index.tsx
+++ src/lib/components/message/index.tsx
@@ -1,14 +1,18 @@
 import { Heading, Box, Image, Text } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import FramerFadeIn from '../common/FramerFadeIn';
+import PrimaryButton from '../common/PrimaryButton';
 import messagePlaceholder from '~/assets/message/message-placeholder.png';
 import { apiService } from '~/lib/api-service';
 import { useAppContext } from '~/lib/contexts/AppContext';
+import { exportAsImage } from '~/lib/utils';
 
 export default function Message() {
   const [message, setMessage] = useState<string>('<p></p>');
   const { auth, activeLoginModal } = useAppContext();
+
+  const cardRef = useRef<HTMLDivElement>(null);
 
   const fetchUserMessage = async () => {
     const userMessage = await apiService.getMessage();
@@ -26,45 +30,62 @@ export default function Message() {
   return (
     <FramerFadeIn>
       {auth.user && (
-        <Box textAlign="center">
-          <Heading
-            color="brand.300"
-            textTransform="uppercase"
-            fontWeight="light"
-          >
-            From us, to you
-          </Heading>
+        <>
           <Box
-            border="1px solid black"
-            borderColor="brand.200"
-            overflow="hidden"
-            borderRadius="full"
-            mt={5}
-            p={3}
-            maxW="300px"
-            mx="auto"
+            textAlign="center"
+            ref={cardRef}
+            bg="rgb(202, 202, 180)"
+            p={2}
+            py={5}
           >
-            <Box borderRadius="full" overflow="hidden">
-              <Image
-                bgPos="center center"
-                src={messagePlaceholder.src}
-                mx="auto"
-              />
+            <Heading
+              color="brand.300"
+              textTransform="uppercase"
+              fontWeight="light"
+            >
+              From us, to you
+            </Heading>
+            <Box
+              border="1px solid black"
+              borderColor="brand.200"
+              overflow="hidden"
+              borderRadius="full"
+              mt={5}
+              p={3}
+              maxW="300px"
+              mx="auto"
+            >
+              <Box borderRadius="full" overflow="hidden">
+                <Image
+                  bgPos="center center"
+                  src={messagePlaceholder.src}
+                  mx="auto"
+                />
+              </Box>
+            </Box>
+            <Box>
+              <Heading color="brand.300" fontWeight="light" my={3}>
+                Dearest {auth.user.name},
+              </Heading>
+              {message && (
+                <Text
+                  fontSize="xs"
+                  color="brand.300"
+                  dangerouslySetInnerHTML={{ __html: message }}
+                />
+              )}
             </Box>
           </Box>
-          <Box>
-            <Heading color="brand.300" fontWeight="light" my={3}>
-              Dearest {auth.user.name},
-            </Heading>
-            {message && (
-              <Text
-                fontSize="xs"
-                color="brand.300"
-                dangerouslySetInnerHTML={{ __html: message }}
-              />
-            )}
+          <Box textAlign="center">
+            <PrimaryButton
+              onClick={() =>
+                exportAsImage(cardRef.current as HTMLDivElement, 'card')
+              }
+            >
+              Save as card
+            </PrimaryButton>
           </Box>
-        </Box>
+        </>
       )}
     </FramerFadeIn>
   );
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,8 +1,28 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import html2canvas from 'html2canvas';
 
 dayjs.extend(relativeTime);
 
 export const getRelativeTime = (time: string) => {
   return dayjs().to(time);
+};
+
+const downloadImage = (blob: string, fileName: string) => {
+  const dummyLink = window.document.createElement('a');
+  dummyLink.download = fileName;
+
+  dummyLink.href = blob;
+
+  document.body.appendChild(dummyLink);
+  dummyLink.click();
+  document.body.removeChild(dummyLink);
+
+  dummyLink.remove();
+};
+
+export const exportAsImage = async (el: HTMLElement, imageFileName: string) => {
+  const canvas = await html2canvas(el);
+  const image = canvas.toDataURL('image/png', 1.0);
+  downloadImage(image, imageFileName);
 };
